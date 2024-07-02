1d8ec13af4a0f8e34cb40a343bbe60930de7a49f
Sufyan908 <bobybutt1048@gmail.com>
2024-07-02 18:51:06 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,19 @@
+# Use of String
+
+mainstring="This is a meessage"
+print(mainstring)
+
+substring=mainstring[0:8]
+print(substring)
+
+newlinestring="This is a message \nThis is new line "
+print(newlinestring)
+
+escapestring="This is a message \"like this\" "
+print(escapestring)
+
+
+print(mainstring[0])
+print(mainstring[2])
+print(mainstring[3])
+
